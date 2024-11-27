92def5ba0ebf52462ad37544826253c4bef38367
Aktarus006 <bastinjoachim@gmail.com>
2024-11-28 00:17:03 +0100

    trying more design slider

--- resources/views/pages/jewels/[id].blade.php
+++ resources/views/pages/jewels/[id].blade.php
@@ -55,19 +55,19 @@ $jewel = Jewel::with(['media', 'collection'])->find(request()->route('id'));
                     prev() {
                         this.activeSlide = (this.activeSlide - 1 + this.totalSlides) % this.totalSlides;
                     }
-                }" class="relative w-full">
-                    <!-- Images -->
-                    <div class="relative w-full h-[80vh] overflow-hidden">
+                }" class="w-full flex">
+                    <!-- Main Image -->
+                    <div class="w-3/4 h-[90vh] relative border-r border-white">
                         @foreach($images as $index => $media)
                             <div x-show="activeSlide === {{ $index }}"
                                  x-transition:enter="transition ease-out duration-300"
-                                 x-transition:enter-start="opacity-0 transform translate-x-full"
-                                 x-transition:enter-end="opacity-100 transform translate-x-0"
+                                 x-transition:enter-start="opacity-0"
+                                 x-transition:enter-end="opacity-100"
                                  x-transition:leave="transition ease-in duration-300"
-                                 x-transition:leave-start="opacity-100 transform translate-x-0"
-                                 x-transition:leave-end="opacity-0 transform -translate-x-full"
+                                 x-transition:leave-start="opacity-100"
+                                 x-transition:leave-end="opacity-0"
                                  class="absolute inset-0">
-                                <img src="{{ $media->getUrl('hd') }}" 
+                                <img src="{{ $media->getUrl('large') }}" 
                                      alt="{{ $jewel->name }}" 
                                      class="w-full h-full object-contain"
                                      loading="{{ $index === 0 ? 'eager' : 'lazy' }}">
@@ -75,13 +75,35 @@ $jewel = Jewel::with(['media', 'collection'])->find(request()->route('id'));
                         @endforeach
                     </div>
 
-                    <!-- Navigation Arrows -->
+                    <!-- Thumbnails -->
+                    <div class="w-1/4 h-[90vh] overflow-y-auto border-l border-white">
+                        @foreach($images as $index => $media)
+                            <button 
+                                @click="activeSlide = {{ $index }}"
+                                class="w-full aspect-square relative group border-b border-white last:border-b-0"
+                                :class="{ 'opacity-50': activeSlide !== {{ $index }} }"
+                            >
+                                <img 
+                                    src="{{ $media->getUrl('thumbnail') }}" 
+                                    alt="{{ $jewel->name }}" 
+                                    class="w-full h-full object-cover"
+                                    loading="lazy"
+                                >
+                                <div 
+                                    class="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-all duration-300"
+                                    :class="{ 'bg-white bg-opacity-10': activeSlide === {{ $index }} }"
+                                ></div>
+                            </button>
+                        @endforeach
+                    </div>
+
+                    <!-- Navigation Arrows (only visible on main image hover) -->
                     @if(count($images) > 1)
-                        <button @click="prev" class="absolute left-4 top-1/2 transform -translate-y-1/2 font-mono text-4xl text-white hover:text-gray-300 bg-black/50 px-4 py-2 backdrop-blur-sm">←</button>
-                        <button @click="next" class="absolute right-4 top-1/2 transform -translate-y-1/2 font-mono text-4xl text-white hover:text-gray-300 bg-black/50 px-4 py-2 backdrop-blur-sm">→</button>
+                        <button @click="prev" class="absolute left-4 top-1/2 transform -translate-y-1/2 font-mono text-4xl text-white hover:text-gray-300 bg-black/50 px-4 py-2 backdrop-blur-sm opacity-0 hover:opacity-100 transition-opacity duration-300">←</button>
+                        <button @click="next" class="absolute right-[calc(25%+1rem)] top-1/2 transform -translate-y-1/2 font-mono text-4xl text-white hover:text-gray-300 bg-black/50 px-4 py-2 backdrop-blur-sm opacity-0 hover:opacity-100 transition-opacity duration-300">→</button>
                         
                         <!-- Slide Counter -->
-                        <div class="absolute bottom-4 left-1/2 transform -translate-x-1/2 font-mono text-white bg-black/50 px-4 py-2 backdrop-blur-sm">
+                        <div class="absolute bottom-4 left-[37.5%] transform -translate-x-1/2 font-mono text-white bg-black/50 px-4 py-2 backdrop-blur-sm">
                             <span x-text="activeSlide + 1"></span>/<span x-text="totalSlides"></span>
                         </div>
                     @endif
