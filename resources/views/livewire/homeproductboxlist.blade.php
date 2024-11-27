ce81c7823aecd8a269c5c33b4da248ab335e1a3c
Aktarus006 <bastinjoachim@gmail.com>
2024-11-27 23:18:24 +0100

    try retrieve reel jewel on home pqge

--- resources/views/livewire/homeproductboxlist.blade.php
+++ resources/views/livewire/homeproductboxlist.blade.php
@@ -1,64 +1,26 @@
 <?php
 
 use function Livewire\Volt\{state, mount, on};
-use Illuminate\Support\Facades\Http;
+use App\Models\Jewel;
 
 state(['jewels' => [], 'selectedJewel' => null]);
 
 mount(function () {
-    // $response = Http::get('/api/jewels/random');
-    // $this->jewels = $response->json();
-    // Fake data
-    $this->jewels = collect([
-        [
-            'id' => 1,
-            'name' => 'Diamond Ring',
-            'price' => 1000,
-            'media' => [['original_url' => 'https://via.placeholder.com/300x300.png?text=Diamond+Ring']],
-        ],
-        [
-            'id' => 2,
-            'name' => 'Gold Necklace',
-            'price' => 800,
-            'media' => [['original_url' => 'https://via.placeholder.com/300x300.png?text=Gold+Necklace']],
-        ],
-        [
-            'id' => 3,
-            'name' => 'Silver Bracelet',
-            'price' => 500,
-            'media' => [['original_url' => 'https://via.placeholder.com/300x300.png?text=Silver+Bracelet']],
-        ],
-        [
-            'id' => 4,
-            'name' => 'Pearl Earrings',
-            'price' => 600,
-            'media' => [['original_url' => 'https://via.placeholder.com/300x300.png?text=Pearl+Earrings']],
-        ],
-        [
-            'id' => 5,
-            'name' => 'Emerald Pendant',
-            'price' => 1200,
-            'media' => [['original_url' => 'https://via.placeholder.com/300x300.png?text=Emerald+Pendant']],
-        ],
-        [
-            'id' => 6,
-            'name' => 'Ruby Bracelet',
-            'price' => 900,
-            'media' => [['original_url' => 'https://via.placeholder.com/300x300.png?text=Ruby+Bracelet']],
-        ],
-        [
-            'id' => 7,
-            'name' => 'Sapphire Ring',
-            'price' => 1100,
-            'media' => [['original_url' => 'https://via.placeholder.com/300x300.png?text=Sapphire+Ring']],
-        ],
-        [
-            'id' => 8,
-            'name' => 'Platinum Watch',
-            'price' => 2000,
-            'media' => [['original_url' => 'https://via.placeholder.com/300x300.png?text=Platinum+Watch']],
-        ],
-    ]);
+    // Get 8 random jewels with their media
+    $this->jewels = Jewel::with('media')
+        ->inRandomOrder()
+        ->take(8)
+        ->get()
+        ->map(function($jewel) {
+            return [
+                'id' => $jewel->id,
+                'name' => $jewel->name,
+                'price' => $jewel->price,
+                'media' => $jewel->media->map(function($media) {
+                    return ['original_url' => $media->getUrl()];
+                })->toArray()
+            ];
+        });
 });
 
 on(['jewel-hovered' => function ($jewel) {
@@ -67,27 +29,39 @@ on(['jewel-hovered' => function ($jewel) {
 
 ?>
 
-<div class="flex w-full h-1/2 bg-slate-900">
-    <div class="flex flex-wrap w-1/3">
+<div class="flex w-full h-1/2 bg-black">
+    <div class="flex flex-wrap w-1/3 border-r-4 border-white">
         @foreach ($jewels->take(4) as $jewel)
-            <div class="w-1/2 h-1/2">
-                <livewire:homeproductbox :$jewel />
+            <div class="w-1/2 h-1/2 border-b-4 border-white last:border-b-0">
+                <livewire:homeproductbox :jewelId="$jewel['id']" />
             </div>
         @endforeach
     </div>
-    <div class="flex w-1/3 border border-white border-1">
-        <div class="flex items-center justify-center w-full">
+    <div class="w-1/3 border-r-4 border-white">
+        <div class="flex items-center justify-center w-full h-full">
             @if ($selectedJewel)
-                <img src="{{ $selectedJewel['media'][0]['original_url'] }}" alt="{{ $selectedJewel['name'] }}" class="w-full h-full object-contain">
+                <div class="w-full h-full relative group">
+                    <img 
+                        src="{{ $selectedJewel['media'][0]['original_url'] }}" 
+                        alt="{{ $selectedJewel['name'] }}" 
+                        class="w-full h-full object-contain"
+                    >
+                    <div class="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-80 transition-opacity duration-300 flex items-end justify-start p-8 opacity-0 group-hover:opacity-100">
+                        <div class="text-white font-mono">
+                            <h3 class="text-2xl uppercase mb-2">{{ $selectedJewel['name'] }}</h3>
+                            <p class="text-xl">€{{ number_format($selectedJewel['price'], 2) }}</p>
+                        </div>
+                    </div>
+                </div>
             @else
-                <div class="text-4xl text-white">NOS PRODUITS</div>
+                <div class="text-4xl text-white font-mono tracking-widest">NOS PRODUITS</div>
             @endif
         </div>
     </div>
     <div class="flex flex-wrap w-1/3">
         @foreach ($jewels->skip(4) as $jewel)
-            <div class="w-1/2 h-1/2">
-                <livewire:homeproductbox :$jewel />
+            <div class="w-1/2 h-1/2 border-b-4 border-white last:border-b-0">
+                <livewire:homeproductbox :jewelId="$jewel['id']" />
             </div>
         @endforeach
     </div>
