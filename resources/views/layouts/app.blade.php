dfe1e13929b57920cd2318c001008da2f3c61a5f
Aktarus006 <bastinjoachim@gmail.com>
2025-06-27 01:08:03 +0200

    title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AXEL Jewelry</title>
+    <title>Axel Englebert Bijoutier</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
