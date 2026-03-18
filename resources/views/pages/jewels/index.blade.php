ff4abdf2ad5cc5c82a887ae5ac40c5dbf72d950c
Aktarus006 <bastinjoachim@gmail.com>
2026-03-19 00:53:30 +0100

    style: enlarge desktop diamond animation by 20%

--- resources/views/pages/jewels/index.blade.php
+++ resources/views/pages/jewels/index.blade.php
@@ -2,10 +2,10 @@
     <!-- Title Section -->
 <div class="relative w-full py-12 overflow-hidden bg-black border-t-8 border-b-8 border-white group">
     <!-- Sophisticated Diamond Animation -->
-    <div class="absolute w-20 h-20 md:w-32 md:h-32 transition-all duration-1000 -translate-y-1/2 border-4 md:border-8 border-white left-6 md:left-16 top-1/2 group-hover:border-red-700 rotate-45 group-hover:rotate-[225deg] flex items-center justify-center">
+    <div class="absolute w-20 h-20 md:w-40 md:h-40 transition-all duration-1000 -translate-y-1/2 border-4 md:border-8 border-white left-6 md:left-16 top-1/2 group-hover:border-red-700 rotate-45 group-hover:rotate-[225deg] flex items-center justify-center">
         <!-- Inner Facet -->
-        <div class="absolute inset-2 md:inset-4 border-2 md:border-4 border-white group-hover:border-red-700 -rotate-90 group-hover:rotate-[180deg] transition-all duration-700"></div>
-        <div class="w-2 md:w-4 h-2 md:h-4 bg-white group-hover:bg-red-700 animate-pulse -rotate-45"></div>
+        <div class="absolute inset-2 md:inset-5 border-2 md:border-4 border-white group-hover:border-red-700 -rotate-90 group-hover:rotate-[180deg] transition-all duration-700"></div>
+        <div class="w-2 md:w-5 h-2 md:h-5 bg-white group-hover:bg-red-700 animate-pulse -rotate-45"></div>
     </div>
 
     <!-- Title -->
