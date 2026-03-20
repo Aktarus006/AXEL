a07ef95c8a905340b95e7e62ebba18b9c773326c
Aktarus006 <bastinjoachim@gmail.com>
2026-03-20 09:31:44 +0100

    feat: enable password reset in Filament admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->id("admin")
             ->path("adminAEJ24eonj")
             ->login()
+            ->passwordReset()
             ->colors([
                 "primary" => Color::Zinc,
             ])
